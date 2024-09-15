85d207da7ad1f6e37c2524c154f4735356ea2a2d
HFall3301 <kdanqq@gmail.com>
2024-09-15 23:36:32 +0300

    Update 1.java

--- Java/1.java
+++ Java/1.java
@@ -5,7 +5,7 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in); //ввод
-        boolean isWork;
+        boolean isNotWork;
         double x, y;
 
         x = 0.0;
@@ -14,46 +14,46 @@ public class Main {
         System.out.println("This program determines, whether a point (x,y) belongs to the closed set D.");
 
         do {
-            isWork = false;
+            isNotWork = false;
             System.out.println("Please enter the x coordinate within +/-" + INPUT_ACCURACY);
             try {
                 x = Double.parseDouble(scan.nextLine());
             } catch (NumberFormatException exception) {
                 System.out.println("Please enter ONLY a number again");
-                isWork = true;
+                isNotWork = true;
             }
-            if (!isWork)
+            if (!isNotWork)
                 if (x < INPUT_ACCURACY && x > -INPUT_ACCURACY) {
-                    isWork = false;
+                    isNotWork = false;
                 }
                 else {
                     System.out.println("Please enter a number again within +/-" + INPUT_ACCURACY);
-                    isWork = true;
+                    isNotWork = true;
                 }
-        } while (isWork);
+        } while (isNotWork);
         do {
-            isWork = false;
+            isNotWork = false;
             System.out.println("Please enter the y coordinate within +/-" + INPUT_ACCURACY);
             try {
                 y = Double.parseDouble(scan.nextLine());
             } catch (NumberFormatException exception) {
                 System.out.println("Please enter ONLY a number again");
-                isWork = true;
+                isNotWork = true;
             }
             if (!isWork)
                 if (y < INPUT_ACCURACY && y > -INPUT_ACCURACY) {
-                    isWork = false;
+                    isNotWork = false;
                 }
                 else {
                     System.out.println("Please enter a number again within +/-" + INPUT_ACCURACY);
-                    isWork = true;
+                    isNotWork = true;
                 }
-        } while (isWork);
-
+        } while (isNotWork);
+        scan.close();
+        
         if ((x + y < 1.0 + INACCUARCY) && (2.0 * x - y < 1.0 + INACCUARCY) && (y + INACCUARCY > 0.0))
             System.out.println("This point belongs to the closed set D.");
         else
             System.out.println("This point doesn't belong to the closed set D.");
-        scan.close();
     }
 }
